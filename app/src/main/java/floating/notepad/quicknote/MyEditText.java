ec01259e8ee4e1962f1218ed7603e7c71f2081f9
Shreyas <skraman1999@gmail.com>
2014-09-27 17:45:28 -0700

    Lines.

---
+++ app/src/main/java/floating/notepad/quicknote/MyEditText.java
@@ -0,0 +1,41 @@
+package floating.notepad.quicknote;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class MyEditText extends EditText {
+
+    private static Paint linePaint;
+
+    static {
+        linePaint = new Paint();
+        linePaint.setColor(Color.BLACK);
+        linePaint.setStyle(Paint.Style.STROKE);
+    }
+
+    public MyEditText(Context context, AttributeSet attributes) {
+        super(context, attributes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Rect bounds = new Rect();
+        int firstLineY = getLineBounds(0, bounds);
+        int lineHeight = getLineHeight();
+        int totalLines = Math.max(getLineCount(), getHeight() / lineHeight);
+
+        for (int i = 0; i < totalLines; i++) {
+            int lineY = firstLineY + i * lineHeight;
+            canvas.drawLine(bounds.left, lineY, bounds.right, lineY, linePaint);
+        }
+
+        super.onDraw(canvas);
+    }
+
+
+}
